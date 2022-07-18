ea163588d100dc203e05e68f52cf1cbb4f3e2744
Jan Dohring <jandohring@hotmail.com>
2022-07-18 11:38:08 -0400

    split tech manager

--- settlers_of_valgard/tech/technology.py
+++ settlers_of_valgard/tech/technology.py
@@ -6,6 +6,7 @@ from settlers_of_valgard.colors import Colors
 from settlers_of_valgard.rank import Freeman
 from settlers_of_valgard.settlement import Settlement
 from settlers_of_valgard.player_info import PlayerInfo
+from events.game_event import GameEvent, add_listener
 
 # tech status
 class TechStatus(OrderedEnum):
@@ -29,36 +30,9 @@ class Technology(StaticObject):
         settlement : Settlement = find_object(Settlement)
         settlement.discovered_technologies.append(self)
         # TODO
-        TechManager.instance.update_status()
+        TechDiscoveredEvent(self).send()
 
-
-
-class TechManager(StaticSingleton):
-    instance = None
-
-    def __init__(self) -> None:
+class TechDiscoveredEvent(GameEvent):
+    def __init__(self, tech) -> None:
         super().__init__()
-
-        self.settlement : Settlement = None
-        self.player_info : PlayerInfo = None
-        TechManager.instance = self
-    
-    def __get_status(self, tech : Technology):
-        if tech in self.settlement.discovered_technologies:
-            return DISCOVERED
-        
-        if tech.required_rank < self.player_info.rank:
-            return LOCKED
-        
-        for other in tech.required_tech:
-            if other not in self.settlement.discovered_technologies:
-                return LOCKED
-        
-        return UNLOCKED
-
-    def update_status(self):
-        self.settlement = find_object(Settlement)
-        self.player_info = find_object(PlayerInfo)
-
-        for tech in Query(Technology).all():
-            tech.status = self.__get_status(tech)
+        self.tech = tech
